f232cef416ffd7a9fe3bb6e029fe90e044ca9b71
jejoonlee <jejoonlee1996@gmail.com>
2022-07-29 17:40:44 +0900

    2회차 이제준 모의고사

--- 2회차/이제준/4_신용카드만들기1.py
+++ 2회차/이제준/4_신용카드만들기1.py
@@ -1,3 +1,29 @@
 import sys
 
 sys.stdin = open("_신용카드만들기1.txt")
+
+T = int(input())
+
+for t in range(1, T + 1):
+    num = map(int, input().split())
+    nums = list(num)
+    # 숫자들을 정수로 변환한 후 리스트에 넣는다
+
+    # index가 홀수면 2를 곱하고 더하는 거지만,
+    # 인덱스는 0으로 시작해서
+    # 반대로 짝수면 2를 곱하고,
+    # 홀수면 그냥 더하기로 했다
+    result = 0
+    for i in range(len(nums)):
+        if (i) % 2 == 1:
+            result += nums[i]
+        else:
+            result += (nums[i] * 2)
+    
+    # 그리고 10으로 나눈 후 나눠 떨어지면 0을 출력하고
+    # 그 외에는 10을 나눠 떨어진 숫자에 빼서,
+    # 그 숫자를 출력했다
+    if result % 10 == 0:
+        print(f'#{t} 0')
+    else:
+        print(f'#{t} {10 - result % 10}')
--- 2회차/이제준/5_신용카드만들기2.py
+++ 2회차/이제준/5_신용카드만들기2.py
@@ -1,3 +1,29 @@
 import sys
 
 sys.stdin = open("_신용카드만들기2.txt")
+
+T = int(input())
+
+lst = []
+for i in range(1, T + 1):
+    nums = (str(input()))
+    nums = list(nums.replace('-',''))
+    # 만약 주어진 카드 번호에 '-' 가 있으면 .replace('-', '') 통해
+    # '-'를 없애기
+
+    lst = []
+    if nums[0] == '3' or nums[0] == '4' or nums[0] == '5' or nums[0] == '6' or nums[0] == '9':
+        lst.append(nums)
+    # 숫자가 3, 4, 5, 6, 9로 시작하면 리스트에 넣어주고
+    else:
+        print(f'#{i} 0')
+    # 그게 아니면 바로 0으로 출력한다
+
+    # 리스트 안에 리스트가 있어서, 리스트 하나를 for 문을 통해 없애고
+    # len()을 통해 리스트 안에 숫자들을 센 후
+    # 값이 16이면 1을 아니면 0을 출력한다
+    for valid in lst:
+        if len(valid) == 16:
+            print(f'#{i} 1')
+        else:
+            print(f'#{i} 0')
